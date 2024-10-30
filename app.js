83f1e6bd6355b167c8b08ac2299698dc14103c23
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-10-29 23:03:29 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import userRoutes  from './routes/usersRoutes.js';
+import cors from  'cors';
+
 
   const app = express();
   const PORT = 3002;
   
   app.use(express.json());
 
+  app.disable('x-powered-by')
 
+ app.use(cors())
 
   app.use('/users', userRoutes); // Rutas para usuarios
   
